811f37def5fae3eda00f68326cc30ee5a893e6ab
AcrylicShrimp <led789zxpp@naver.com>
2020-05-27 00:26:20 +0900

    FIX: Add new launch code.

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,12 @@
 mod network;
+mod world;
 
 fn main() {
-    network::server::Server::new().start("0.0.0.0", 19980);
+    network::server::start(
+        std::sync::Arc::new(std::sync::Mutex::new(Vec::new())),
+        std::sync::Arc::new(std::sync::Mutex::new(world::world::World::new())),
+        std::sync::Arc::new(std::sync::Mutex::new(network::handler::Handler::new())),
+        "127.0.0.1",
+        19980,
+    );
 }
