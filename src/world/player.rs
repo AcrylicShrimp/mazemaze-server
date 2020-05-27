7b85132012909abc9ea2a3e745454b15857648ac
AcrylicShrimp <led789zxpp@naver.com>
2020-05-28 01:16:50 +0900

    FEAT: Add a random color generation.

--- src/world/player.rs
+++ src/world/player.rs
@@ -1,5 +1,7 @@
 use super::object;
 
+use byteorder::WriteBytesExt;
+
 pub struct Player {
 	id: u64,
 	glyph: char,
@@ -9,10 +11,18 @@ pub struct Player {
 
 impl Player {
 	pub fn new(id: u64, x: i32, y: i32) -> Player {
+		let mut bytes = vec![];
+
+		bytes.write_u64::<byteorder::LittleEndian>(id).unwrap();
+
 		Player {
 			id,
 			glyph: '@',
-			color: (0, 127, 255),
+			color: (
+				std::cmp::min(bytes[0] as u32 + 64, 255) as u8,
+				std::cmp::min(bytes[1] as u32 + 64, 255) as u8,
+				std::cmp::min(bytes[2] as u32 + 64, 255) as u8,
+			),
 			object: object::Object::new(x, y),
 		}
 	}
